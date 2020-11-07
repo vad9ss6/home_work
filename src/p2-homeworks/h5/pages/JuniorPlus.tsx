fc98254f720211764723876a56998b11592222d9
vad9ss6 <vad9ss6@gamail.com>
2020-11-07 23:53:28 +0300

    added component HW6

--- src/p2-homeworks/h5/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import HW6 from "../../h6/HW6";
 
 
 function JuniorPlus() {
     return (
         <div>
             <h1>Hello world</h1>
-            {/*<HW6/>*/}
+            <HW6/>
 
         </div>
     );
