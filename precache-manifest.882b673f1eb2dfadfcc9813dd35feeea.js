4d7129e1102c7ca15c929dcd632876f6fadd0d41
vad9ss6 <vad9ss6@gamail.com>
2020-11-07 15:34:38 +0300

    Updates

---
+++ precache-manifest.882b673f1eb2dfadfcc9813dd35feeea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776f9fe58ffdeccd46253a557c3963ca",
+    "url": "/simple-react-homeworks-master/index.html"
+  },
+  {
+    "revision": "51440df3261d8642dcbf",
+    "url": "/simple-react-homeworks-master/static/css/main.205ce793.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/simple-react-homeworks-master/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-homeworks-master/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51440df3261d8642dcbf",
+    "url": "/simple-react-homeworks-master/static/js/main.90131a7d.chunk.js"
+  },
+  {
+    "revision": "33f020c42a00208bc7a5",
+    "url": "/simple-react-homeworks-master/static/js/runtime-main.074d676d.js"
+  }
+]);
