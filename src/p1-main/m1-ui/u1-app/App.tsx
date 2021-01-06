eae25f4767f82da70229875d9b02039a6902afe9
vad9ss6 <vad9ss6@gmail.com>
2021-01-06 23:13:46 +0300

    home work 9

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 import s from "./App.module.css";
-import HW1 from "../../../p2-homeworks/h1/HW1";
-import HW2 from "../../../p2-homeworks/h2/HW2";
-import HW3 from "../../../p2-homeworks/h3/HW3";
-import HW4 from "../../../p2-homeworks/h4/HW4";
 import HW5 from "../../../p2-homeworks/h5/HW5";
 import HW7 from "../../../p2-homeworks/h7/HW7";
 import HW8 from "../../../p2-homeworks/h8/HW8";
@@ -11,22 +7,11 @@ import {Provider} from "react-redux";
 import {store} from "../../../p2-homeworks/h8/bll/redux/redux-store";
 import HW9 from "../../../p2-homeworks/h9/HW9";
 
+
 function App() {
     return (
         <div className={s.App}>
-
-            {/*<HW1/>*/}
-            {/*<HW2/>*/}
-            {/*<HW3/>*/}
-            {/*<HW4/>*/}
-            {/*<HW5/>*/}
-            {/*<HW6/>*/}
-            {/*<HW7/>*/}
-            <Provider store={store}>
-                <HW8/>
-            </Provider>
-            {/*<HW9/>*/}
-
+            <HW5/>
 
         </div>
     );
--- src/p2-homeworks/h5/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import HW6 from "../../h6/HW6";
+import HW7 from "../../h7/HW7";
+import {Provider} from "react-redux";
+import {store} from "../../h8/bll/redux/redux-store";
+import HW8 from "../../h8/HW8";
+import HW9 from "../../h9/HW9";
+import HW10 from "../../h10/HW10";
 
 
 function JuniorPlus() {
@@ -7,7 +13,12 @@ function JuniorPlus() {
         <div>
             <h1>Hello world</h1>
             <HW6/>
-
+            <HW7/>
+            <Provider store={store}>
+                <HW8/>
+            </Provider>
+            <HW9/>
+            <HW10 />
         </div>
     );
 }
